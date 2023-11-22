b611bfc091b56d1c78ec589662a4c0ba57f8c285
Ngô Xuân Trọng <trongnx@mor.com.vn>
2023-11-22 14:08:10 +0700

    initial commit

---
+++ lib/constants/utils.dart
@@ -0,0 +1,48 @@
+import 'dart:io';
+
+import 'package:file_picker/file_picker.dart';
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:permission_handler/permission_handler.dart';
+
+void showSnackBar(BuildContext context, String text) {
+  ScaffoldMessenger.of(context).showSnackBar(
+    SnackBar(
+      content: Text(text),
+    ),
+  );
+}
+
+List<File> images = [];
+Future<List<XFile>> selectImages2() async {
+  List<XFile> imageFileList = [];
+  final List<XFile>? selectedImages = await ImagePicker().pickMultiImage();
+  if (selectedImages!.isNotEmpty) {
+    imageFileList.addAll(selectedImages);
+  }
+  return imageFileList;
+  // setState(() {});
+}
+
+Future<List<File>> pickImages() async {
+  List<File> images = [];
+  try {
+    var status = await Permission.manageExternalStorage.request();
+    if (status.isGranted) {
+      var files = await FilePicker.platform.pickFiles(
+        type: FileType.image,
+        allowMultiple: true,
+      );
+      if (files != null && files.files.isNotEmpty) {
+        for (int i = 0; i < files.files.length; i++) {
+          images.add(File(files.files[i].path!));
+        }
+      }
+    } else if (status.isPermanentlyDenied) {
+      openAppSettings();
+    }
+  } catch (e) {
+    debugPrint(e.toString());
+  }
+  return images;
+}
