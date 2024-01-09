0e7d5ee389a1ad9883a1a99099bd9f3a471a5611
Ngô Xuân Trọng <trongnx@mor.com.vn>
2024-01-09 17:34:39 +0700

    9/1

--- lib/features/account/widgets/top_buttons.dart
+++ lib/features/account/widgets/top_buttons.dart
@@ -1,4 +1,6 @@
-import 'package:amazon_clone_tutorial/common/widgets/custom_dialog.dart';
+import 'package:amazon_clone_tutorial/common/widgets/popup_notification_custom.dart';
+import 'package:amazon_clone_tutorial/features/account/screens/change_password_screen.dart';
+import 'package:amazon_clone_tutorial/features/account/screens/my_orders_screen.dart';
 import 'package:amazon_clone_tutorial/features/account/screens/update_profile_screen.dart';
 import 'package:amazon_clone_tutorial/features/account/services/account_services.dart';
 import 'package:amazon_clone_tutorial/features/account/widgets/account_button.dart';
@@ -11,38 +13,39 @@ class TopButtons extends StatelessWidget {
   Widget build(BuildContext context) {
     return Column(
       children: [
-        // Row(
-        //   children: [
-        //     AccountButton(
-        //       text: 'Đơn hàng của bạn',
-        //       onTap: () {},
-        //     ),
-        //     AccountButton(
-        //       text: 'Turn Seller',
-        //       onTap: () {},
-        //     ),
-        //   ],
-        // ),
-        // const SizedBox(height: 10),
         Row(
           children: [
+            AccountButton(
+              text: 'Đơn hàng của tôi',
+              onTap: () {
+                Navigator.pushNamed(context, MyOrdersScreen.routeName);
+              },
+            ),
             AccountButton(
               text: 'Thông tin cá nhân',
               onTap: () {
                 Navigator.pushNamed(context, UpdateProfileScreen.routeName);
               },
             ),
+          ],
+        ),
+        const SizedBox(height: 10),
+        Row(
+          children: [
+            AccountButton(
+              text: 'Đổi mật khẩu',
+              onTap: () {
+                Navigator.pushNamed(context, ChangePasswordScreen.routeName);
+              },
+            ),
             AccountButton(
               text: 'Đăng xuất',
               onTap: () {
-                showDialog(
+                PopupNotificationCustom.showMessgae(
                   context: context,
-                  builder: (mContext) {
-                    return CustomDialog(
-                      title: "Bạn có chắc muốn đăng xuất?",
-                      pressAgreeButton: () => AccountServices().logOut(context),
-                    );
-                  },
+                  title: 'ĐĂNG XUẤT',
+                  message: 'Bạn có thực sự muốn thoát phiên đăng nhập này?',
+                  pressButtonLeft: () => AccountServices().logOut(context),
                 );
               },
             ),
