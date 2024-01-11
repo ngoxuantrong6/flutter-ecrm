f2c81a55a138cc945fc045050e380e4d6b2f05d3
Ngô Xuân Trọng <trongnx@mor.com.vn>
2024-01-11 16:35:27 +0700

    fix

--- middlewares/admin.js
+++ middlewares/admin.js
@@ -13,8 +13,8 @@ const admin = async (req, res, next) => {
         .status(400)
         .json({ msg: "Token verification failed, authorization denied." });
     const user = await User.findById(verified.id);
-    if (user.type == "user" || user.type == "seller") {
-      return res.status(401).json({ msg: "You are not an admin!" });
+    if (user.type == "user" || user.type == "admin") {
+      return res.status(400).json({ msg: "You are not an admin!" });
     }
     req.user = verified.id;
     req.token = token;
