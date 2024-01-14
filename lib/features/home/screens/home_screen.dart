236de29b13f5603ff47ae104c8e8b0eb7267ccad
Trong <trongnx@mor.com.vn>
2024-01-14 12:07:17 +0700

    14/1

--- lib/features/home/screens/home_screen.dart
+++ lib/features/home/screens/home_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:amazon_clone_tutorial/constants/global_variables.dart';
 import 'package:amazon_clone_tutorial/features/home/widgets/address_box.dart';
 import 'package:amazon_clone_tutorial/features/home/widgets/carousel_image.dart';
@@ -6,8 +8,10 @@ import 'package:amazon_clone_tutorial/features/home/widgets/top_categories.dart'
 import 'package:amazon_clone_tutorial/features/search/screens/search_screen.dart';
 import 'package:avatar_glow/avatar_glow.dart';
 import 'package:flutter/material.dart';
-import 'package:highlight_text/highlight_text.dart';
+import 'package:provider/provider.dart';
+import 'package:speech_to_text/speech_recognition_event.dart';
 import 'package:speech_to_text/speech_to_text.dart' as stt;
+import 'package:speech_to_text/speech_to_text_provider.dart';
 
 class HomeScreen extends StatefulWidget {
   static const String routeName = '/home';
@@ -19,15 +23,15 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   TextEditingController searchTextController = TextEditingController();
-  stt.SpeechToText _speech = stt.SpeechToText();
+  late SpeechToTextProvider speechToTextProvider;
   bool _isListening = false;
   String _text = '';
   String _hintText = 'Tìm kiếm';
 
   @override
-  void initState() {
-    super.initState();
-    _speech = stt.SpeechToText();
+  void dispose() {
+    searchTextController.clear();
+    super.dispose();
   }
 
   void navigateToSearchScreen(String query) {
@@ -36,6 +40,7 @@ class _HomeScreenState extends State<HomeScreen> {
 
   @override
   Widget build(BuildContext context) {
+    speechToTextProvider = Provider.of<SpeechToTextProvider>(context);
     searchTextController.text = _text;
     return Scaffold(
       appBar: PreferredSize(
@@ -136,38 +141,85 @@ class _HomeScreenState extends State<HomeScreen> {
     );
   }
 
-  void _listen() async {
-    if (!_isListening) {
-      bool available = await _speech.initialize(
-        onStatus: (val) {
-          print('onStatus: $val');
-          if (val == stt.SpeechToText.listeningStatus) {
-            _hintText = 'Đang nghe';
-          }
-          if (val == stt.SpeechToText.doneStatus) {
-            setState(() => _isListening = false);
-            print("vào doneeeeeeeee");
+  // void _listen() async {
+  //   if (!_isListening) {
+  //     bool available = await _speech.initialize(
+  //       onStatus: (val) {
+  //         print('onStatus: $val');
+  //         if (val == stt.SpeechToText.listeningStatus) {
+  //           // _hintText = 'Đang nghe';
+  //           setState(() => _hintText = 'Đang nghe');
+  //         }
+  //         if (val == stt.SpeechToText.doneStatus) {
+  //           // setState(() => _isListening = false);
+  //           setState(() => print("ggggggggggggg"));
+  //           _stopListening();
 
-            navigateToSearchScreen(_text);
-          }
-        },
-        onError: (val) => print('onError: $val'),
-      );
+  //           navigateToSearchScreen(_text);
+  //         }
+  //       },
+  //       onError: (val) => print('onError: $val'),
+  //     );
 
-      if (available) {
-        setState(() => _isListening = true);
-        _speech.listen(
-          onResult: (val) => setState(() {
-            _text = val.recognizedWords;
-            // if (val.hasConfidenceRating && val.confidence > 0) {
-            //   _confidence = val.confidence;
-            // }
-          }),
-        );
-      }
-    } else {
-      setState(() => _isListening = false);
-      _speech.stop();
-    }
+  //     if (available) {
+  //       setState(() => _isListening = true);
+  //       _speech.listen(
+  //         onResult: (val) => setState(() {
+  //           _text = val.recognizedWords;
+  //           // if (val.hasConfidenceRating && val.confidence > 0) {
+  //           //   _confidence = val.confidence;
+  //           // }
+  //         }),
+  //       );
+  //     }
+  //   } else {
+  //     _stopListening();
+  //   }
+  // }
+
+  void _listen() {
+    speechToTextProvider.listen(
+        pauseFor: const Duration(seconds: 5),
+        // localeId: 'en-us',
+        listenFor: const Duration(seconds: 5));
+    speechToTextProvider.stream.listen(
+      (event) {
+        print("vào đây");
+        // on listening starts
+        if (event.eventType == SpeechRecognitionEventType.statusChangeEvent) {
+          _hintText = 'Đang nghe';
+          if (mounted) {
+            setState(() => _isListening = true);
+          }
+        }
+        // on every change it update
+        if (event.eventType ==
+            SpeechRecognitionEventType.partialRecognitionEvent) {
+          if (mounted) {
+            setState(
+              () {
+                _text = speechToTextProvider.lastResult!
+                    .recognizedWords; // the textField or Text Widget Will be updated
+              },
+            );
+          }
+        }
+        //on error
+        else if (event.eventType == SpeechRecognitionEventType.errorEvent) {
+          ///on error if some error occurs then close the dilog box here . or stop the listner
+          print('onError');
+        }
+
+        //on done
+        else if (event.eventType == SpeechRecognitionEventType.doneEvent) {
+          //when the user stop speaking.
+          print("vào onDoneeeeeee");
+          speechToTextProvider.cancel();
+          speechToTextProvider.stop();
+          setState(() => _isListening = false);
+          navigateToSearchScreen(_text);
+        }
+      },
+    );
   }
 }
